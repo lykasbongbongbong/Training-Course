e65bd64751036e351a8933957cf11d30c2fd301f
lykasbongbongbong <lykaslin1022@gmail.com>
2021-04-13 22:45:19 +0800

    bug fixed

--- segmentation-detection/segmentation.py
+++ segmentation-detection/segmentation.py
@@ -5,6 +5,6 @@ fcn = models.segmentation.fcn_resnet101(pretrained=True).eval()
 from PIL import Image
 import matplotlib.pyplot as plt
 import torch
-!wget -nv https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/10/19/pinyon-jay-bird.jpg -O bird.png
+#!wget -nv https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/10/19/pinyon-jay-bird.jpg -O bird.png
 img = Image.open('./bird.png')
 plt.imshow(img); plt.show()
